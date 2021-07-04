77938586fbbefd63cda0559d46ddb83d49e471a9
sadra <sadrataghavi1383@gmail.com>
2021-07-04 17:43:31 +0430

    adding url pathes for detail and create todo objects

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path("", apiOverview, name="apiOverview"),
     path("task-list/", taskList, name="task-list"),
+    path("task-detail/<str:pk>/", taskDetail, name="task-detail"),
 ]
 
 
