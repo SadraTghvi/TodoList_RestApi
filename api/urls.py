d38e278e44e1ef6d4220408d872b07bcb5fe92fa
sadra <sadrataghavi1383@gmail.com>
2021-07-04 14:41:59 +0430

    serializer file added / urls updated

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,8 @@ from api.views import *
 from django.urls import path, include
 
 urlpatterns = [
-    path("", apiOverview, name="apiOverview")
+    path("", apiOverview, name="apiOverview"),
+    path("task-list/", taskList, name="task-list"),
 ]
 
 
