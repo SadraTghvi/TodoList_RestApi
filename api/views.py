dc45dc2e2d46309976f6acf82f6805f642a61155
sadra <sadrataghavi1383@gmail.com>
2021-07-04 14:39:09 +0430

    function for showing task model objects

--- api/views.py
+++ api/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from api.models import Task
+from api.serializers import TaskSerializer
 
 # Create your views here.
 
@@ -19,3 +21,10 @@ def apiOverview(request):
         "Delete": "/task-delete/<str:pk>/",
     }
     return Response(api_urls)
+
+
+@api_view(["GET"])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks,many=True)
+    return Response(serializer.data)
