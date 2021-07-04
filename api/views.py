95941a1477ccc720c88c0d16f0685bd29e9feb0e
sadra <sadrataghavi1383@gmail.com>
2021-07-04 14:18:37 +0430

    importing rest framework to our view and checking out how it works(its so cool)

--- api/views.py
+++ api/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
 def home(request):
     return render(request,"base.html")
+
+@api_view(["GET"])
+def apiOverview(request):
+    api_urls = {
+        "List": "/task-view/",
+        "Detail View": "/task-detail/<str:pk>/",
+        "Create": "/task-create/",
+        "Update": "/task-update/<str:pk>/",
+        "Delete": "/task-delete/<str:pk>/",
+    }
+    return Response(api_urls)
