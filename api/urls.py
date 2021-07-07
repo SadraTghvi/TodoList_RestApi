546b54d1b73ab3ae9678a9cd3fef2a51b118afad
sadra <sadrataghavi1383@gmail.com>
2021-07-07 13:39:03 +0430

    added urls for delete/create

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path("", apiOverview, name="apiOverview"),
     path("task-list/", taskList, name="task-list"),
     path("task-detail/<str:pk>/", taskDetail, name="task-detail"),
+    path("task-create/", taskCreate, name="task-create"),
+    path("task-update/", taskUpdate, name="task-update"),
+    path("task-delete/", taskDelete, name="task-delete"),
 ]
 
 
